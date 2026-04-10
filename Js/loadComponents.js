7cd1932ef7b199168aed41af5ae7eef38ef04ce2
CarlosDeUganda <carlosdeuganda@gmail.com>
2026-04-10 22:36:07 +0300

    Update loadComponents.js

--- Js/loadComponents.js
+++ Js/loadComponents.js
@@ -1,1 +1,9 @@
+async function loadComponent(id, file) {
+  const res = await fetch(file);
+  const data = await res.text();
+  document.getElementById(id).innerHTML = data;
+}
 
+// Load header and footer in every page
+loadComponent("header", "components/header.html");
+loadComponent("footer", "components/footer.html");
